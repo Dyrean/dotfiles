ff0fe121d914fff50d4f320e3bbfc8f2dd693e4a
Dyrean <fcanbeyy@gmail.com>
2024-06-26 00:13:37 +0300

    dotfiles changed, todo: nvim

--- dotfiles/nvim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Split screen
-vim.keymap.set("n", "<leader>wv", ":split<Return>", { desc = "[w]indow [v]ertical Split", noremap = true, silent = true })
-vim.keymap.set("n", "<leader>wh", ":vsplit<Return>", { desc= "[w]indow [h]orizontal Split", noremap = true, silent = true })
--- dotfiles/wezterm/.config/wezterm/wezterm.lua
+++ dotfiles/wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback { "GeistMono Nerd Font", "JetBrainsMono Nerd Font" }
-config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font_with_fallback({ "GeistMono Nerd Font", "JetBrainsMono Nerd Font" })
+config.color_scheme = "Tokyo Night Storm"
 
 -- config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = false
 
-local wayland_gnome = require 'wayland_gnome'
+local wayland_gnome = require("wayland_gnome")
 wayland_gnome.apply_to_config(config)
 
 return config
