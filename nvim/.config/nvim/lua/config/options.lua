a62b2b9bbc4a9eb468dcf564eac327898f015493
Dyrean <fcanbeyy@gmail.com>
2024-07-06 13:40:48 +0300

    revert to 77d14cfb02e3a79e0a71641336048b300be598ce

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,29 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
-vim.opt.conceallevel = 1
-vim.opt.cursorline = false
-vim.opt.number = true -- Print line number
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.hlsearch = false -- highlight search
-vim.opt.incsearch = true -- incremental search
-vim.opt.scrolloff = 4 -- scroll offset
-vim.opt.clipboard = "unnamedplus" -- sync clipboard with os
-vim.opt.breakindent = true
-vim.opt.inccommand = "split"
-
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.showmode = false
 
-vim.opt.swapfile = false
+vim.g.have_nerd_font = true
 
-vim.opt.cinoptions:append(":0") -- switch statement indentations
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.opt.title = true
-vim.opt.ignorecase = true
+vim.opt.hlsearch = true
 
-vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.opt.path:append({ "**" })
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,10 +2,34 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      nil_ls = {},
       lua_ls = {},
       bashls = {},
-      mesonlsp = {},
+      eslint = {},
+    },
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
     },
   },
 }
