24e413bb2dc50782da850b4b8fe2f0a6bfce2baa
Dyrean <fcanbeyy@gmail.com>
2024-07-06 14:41:03 +0300

    nvim

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,35 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      lua_ls = {},
+      bashls = {},
+      eslint = {},
+    },
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
